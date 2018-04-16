a631f6f25cd4ab028af402483ecaf67ac74745aa
Stephen Zimmerman <stephenzimmerman@Stephens-Macbook-Pro-2.local>
2018-04-16 15:36:39 -0700

    updated main and makefile

---
+++ main.c
@@ -0,0 +1,626 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// Serial init
+int state = 0;
+int flag = 0;
+int sendMoneyFlag = 0;
+volatile int key = 10;
+int correct = 0;//correct MUST be 4 when the right numbers are pressed
+
+volatile int lastKey = 10;
+int initializationFlag = 0;
+int passcodeKey1 = 0;
+int passcodeKey2 = 2;
+int passcodeKey3 = 7;
+int passcodeKey4 = 9;
+
+//state = 0 | ENTER PASSWORD
+//state = 1 | HOME SCREEN
+//state = 2 | SEND MONEY
+//etc, add states as needed
+
+void serial_init ( unsigned short ubrr ) {
+	UBRR0 = ubrr ; // Set baud rate
+	UCSR0B |= (1 << TXEN0 ); // Turn on transmitter
+	UCSR0B |= (1 << RXEN0 ); // Turn on receiver
+	UCSR0C = (3 << UCSZ00 ); // Set for async . operation , no parity ,
+	// one stop bit , 8 data bits
+}
+
+//serial_out - Output a byte to the USART0 port
+void serial_out ( char ch )
+{
+	while (( UCSR0A & (1 << UDRE0 )) == 0);
+	UDR0 = ch ;
+}
+
+//serial_in - Read a byte from the USART0 and return it
+char serial_in ()
+{
+while ( !( UCSR0A & (1 << RXC0 )) );
+PORTC |= 1 << PC0;
+return UDR0 ;
+}
+
+
+void clearscreen()
+{
+	serial_out(254);
+	serial_out(81);
+	_delay_ms(10);
+}
+void passcode()
+{
+	if(key==passcodeKey1)
+	{
+		if (flag ==0)
+			{
+				correct++;
+				flag = 1;
+			}
+		lastKey = key;
+	}
+
+	if(key == passcodeKey2)
+	{
+		if (lastKey == passcodeKey1)
+			{
+				correct++;
+
+			}
+		else if(lastKey == passcodeKey2)
+		{
+			//do nothing
+		}
+		else if(lastKey != passcodeKey1)
+		{
+			correct = 0;
+		}
+		lastKey = key;
+		flag = 0;
+	}
+
+
+	if(key == passcodeKey3)
+	{
+		if (lastKey == passcodeKey2)
+			{
+				correct++;
+
+		}
+		else if(lastKey == passcodeKey3)
+		{
+			//do nothing
+		}
+		else if(lastKey !=passcodeKey2)
+		{
+			correct = 0;
+		}
+		lastKey = key;
+		flag = 0;
+	}
+
+
+	if(key == passcodeKey4)
+	{
+		if (lastKey == passcodeKey3)
+			{
+				correct++;
+
+		}
+		else if(lastKey == passcodeKey4)
+		{
+			//do nothing
+		}
+		else if(lastKey !=passcodeKey3)
+		{
+			correct = 0;
+		}
+		lastKey = key;
+		flag = 0;
+	}
+
+	else if ((key!=passcodeKey1) && (key!=passcodeKey2) && (key!=passcodeKey3) && (key!=passcodeKey4))
+	{
+		correct = 0;
+		//serial_out(correct+48);
+		flag = 0;
+	}
+
+}
+int main(void)
+{
+	int welcomeFlag = 0;
+	int i;
+  // Call init sequence
+	serial_init(47);
+	DDRC |= 1 << DDC0;
+	clearscreen();
+
+
+  ///interuppt code
+    DDRD &= ~(1 << DDD3);     // Clear the PD2 pin
+    // PD2 (PCINT0 pin) is now an input
+    PORTD |= (1 << PORTD3);    // turn On the Pull-up
+    // PD2 is now an input with pull-up enabled
+    EICRA	=	0x02; //TRIGGER ON FALLING EDGE
+    //EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
+    EIMSK |= (1 << INT1);     // Turns on INT1
+    sei();
+  //////
+
+
+
+
+  //ENABLE INTERRUPTS TO CALL keypadScan()
+
+
+  // Set all keypad columns as outputs
+  DDRB |= 1 << DDB0; // 1,4,7 Column
+  DDRD |= 1 << DDD6; // 2,5,8 Column
+  DDRD |= 1 << DDD7; // 3,6,9 Column
+
+  PORTD &= ~(1 << PD7);
+  PORTD &= ~(1 << PD6);
+  PORTB &= ~(1 << PB0);
+
+  DDRC |= 1 << DDD0; //LED set for output
+  // Set all keypad rows as inputs
+  DDRD &= ~(1 << DDD5); // 1,2,3 Row
+  DDRB &= ~(1 << DDB7); // 4,5,6 Row
+  DDRB &= ~(1 << DDB1); // 7,8,9 Row
+  DDRB &= ~(1 << DDB2); // *,0,# Row
+  // Pull-up row inputs
+  PORTD |= (1<<PD5);
+  PORTB |= (1<<PB7);
+  PORTB |= (1<<PB1);
+  PORTB |= (1<<PB2);
+
+
+
+	/*
+	if (initializationFlag == 0)
+	{
+		state = 10;
+
+	}
+	if (initializationFlag == 1)
+	{
+		state = 0;
+	}
+
+*/
+
+while (1)
+{
+	while(1)
+	{
+  switch(state)
+  {
+		/*
+		case 10:
+		{
+			serial_out(254);
+			serial_out(81);
+			_delay_ms(10);
+			char word[] = "To begin, create your 4 digit passcode";
+			for(i = 0; word[i] != 0; i++)
+			{
+				serial_out(word[i]);
+			}
+			_delay_ms(500);
+
+			serial_out(254);
+			serial_out(81);
+			_delay_ms(10);
+
+			char word9[] = "Enter the first key";
+			for(i = 0; word9[i] != 0; i++)
+			{
+				serial_out(word9[i]);
+			}
+			while(key==10)
+			{
+
+			}
+			passcodeKey1 = key;
+			serial_out(passcodeKey1+48);
+			_delay_ms(2000);
+			initializationFlag = 1;
+			key = 10;
+			state = 0;
+		}
+		*/
+    case 0:
+		{
+      //prompt user to begin entering passcode
+					i = 0; //just to prevent error of Declaration after Statement, not allowed in C
+					clearscreen();
+			    char word[] = "Enter Passcode";
+			    for(i = 0; word[i] != 0; i++)
+			    {
+			      serial_out(word[i]);
+			    }
+			    //serial_out(word);
+			    _delay_ms(1);
+
+
+		 while (correct!=4)
+					{
+						//serial_out(correct+48);
+						_delay_ms(1);
+						passcode();
+					}
+
+
+					for(i = 0; i < 5; i++)
+					{
+					PORTC |= 1 << PC0;
+					_delay_ms(100);
+					PORTC &= 0 << PC0;
+					_delay_ms(100);
+
+
+					clearscreen();
+
+				char word1[] = "Passcode Correct";
+				for(i = 0; word1[i] != 0; i++)
+				{
+				serial_out(word1[i]);
+				}
+				//serial_out(word);
+				_delay_ms(1000);
+				state = 1;
+				break;
+			}
+			}
+
+
+			case 1: //CASE 1: HOME SCREEN.
+			{
+			//reset key to 0
+			//clear screen
+			clearscreen();
+			key = 0;
+
+			if (welcomeFlag ==0 )
+			{
+			char word2[] = "Welcome";
+			for(i = 0; word2[i] != 0; i++)
+			{
+				serial_out(word2[i]);
+			}
+			_delay_ms(1000);
+			welcomeFlag = 1;
+		}
+
+
+			//CLEAR SCREEN HERE
+			clearscreen();
+			key = 0;
+
+
+			char word[] = "Please select";
+			char word1[] = "an option";
+			for(i = 0; word[i] != 0; i++)
+			{
+				serial_out(word[i]);
+			}
+			//NEW LINE
+			serial_out(0xFE);
+			serial_out(0x45);
+			serial_out(0x40);
+
+			for(i = 0; word1[i] != 0; i++)
+			{
+				serial_out(word1[i]);
+			}
+			strcpy(word, "2: Send");
+			strcpy(word1, "3: Recieve");
+
+			serial_out(0xFE);
+			serial_out(0x45);
+			serial_out(0x14);
+
+			for(i = 0; word[i] != 0; i++)
+			{
+				serial_out(word[i]);
+			}
+
+			serial_out(0xFE);
+			serial_out(0x45);
+			serial_out(0x54);
+
+			for(i = 0; word1[i] != 0; i++)
+			{
+				serial_out(word1[i]);
+			}
+
+
+			while (key == 0)
+			{
+
+			}
+			_delay_ms(1);
+			state = key;
+			break;
+
+		}
+
+
+
+		case 2:
+			{
+				clearscreen();
+				char word2[] = "Send Money";
+				for(i = 0; word2[i] != 0; i++)
+				{
+					serial_out(word2[i]);
+				}
+				_delay_ms(1000);
+
+				clearscreen();
+				char word3[] = "Enter the amount";
+
+				for(i = 0; word3[i] != 0; i++)
+				{
+					serial_out(word3[i]);
+				}
+				strcpy(word3, "you'd like to send");
+				serial_out(0xFE);
+				serial_out(0x45);
+				serial_out(0x40);
+
+				for(i = 0; word3[i] != 0; i++)
+				{
+					serial_out(word3[i]);
+				}
+				sendMoneyFlag = 1;
+				//ISR routine takes car of sending to Pi
+
+				serial_out(0xFE);
+				serial_out(0x45);
+				serial_out(0x54);
+
+				while(key!=12)
+				{
+					//do nothing
+				}
+				clearscreen();
+
+
+				strcpy(word3, "Tap for Address Now");
+				for(i = 0; word3[i] != 0; i++)
+				{
+					serial_out(word3[i]);
+				}
+
+				//CREATE ROUTINE TO GET ADDRESSS
+				//ACTIVATE FLAG PIN FOR PI
+				//CHANGE SERIAL MUX
+				//SEND DATA TO PI
+
+				_delay_ms(1000);
+				clearscreen();
+				strcpy(word3, "sending");
+				for(i = 0; word3[i] != 0; i++)
+				{
+					serial_out(word3[i]);
+				}
+				for(i = 0; i< 6; i++)
+				{
+				serial_out(0x2E);
+				_delay_ms(500);
+			}
+
+
+
+				clearscreen();
+				strcpy(word3, "done");
+				for(i = 0; word3[i] != 0; i++)
+				{
+					serial_out(word3[i]);
+				}
+				_delay_ms(1000);
+				sendMoneyFlag = 0;
+				state = 1;
+				break;
+			}
+			case 3:
+				{
+					clearscreen();
+					char word2[] = "State 3";
+					for(i = 0; word2[i] != 0; i++)
+					{
+						serial_out(word2[i]);
+					}
+					_delay_ms(2000);
+					state = 1;
+					break;
+				}
+
+				default:
+				{
+					clearscreen();
+					char word2[] = "not a valid state";
+					for(i = 0; word2[i] != 0; i++)
+					{
+						serial_out(word2[i]);
+					}
+					_delay_ms(2000);
+					state = 1;
+					break;
+				}
+
+}
+}
+}
+    return 0;   /* never reached */
+}
+
+ISR(INT1_vect)
+{
+  PORTD |= 1 << PD7;
+  PORTD |= 1 << PD6;
+  PORTB &= ~(1 << PB0);
+  // Hold Column 1,4,7,* at 1
+  if ((PIND & (1 << PD5)) == 0)  //1 is pressed
+      {
+
+        key = 1;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+      }
+    //record 1
+
+  if ((PINB & (1 << PB7)) == 0)  //4 is pressed
+      {
+
+        key = 4;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+      } //record 1
+  if ((PINB & (1 << PB1)) == 0)  //7 is pressed
+      {
+
+        key = 7;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+
+      } //record 7
+	if ((PINB & (1 << PB2)) == 0)  //* is pressed
+			{
+
+				key = 11; //11 will be a period
+				PORTC |= 1 << PC0;
+				_delay_ms(200);
+				PORTC &= 0 << PC0;
+
+
+			} //record .
+
+
+
+  PORTD &= ~(1 << PD7);
+  PORTD |= 1 << PD6;
+  PORTB |= 1 << PB0;
+  if ((PIND & (1 << PD5)) == 0)//2 is pressed
+      {
+
+        key = 2;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+
+      } //record 2
+  if ((PINB & (1 << PB7)) == 0) //5 is pressed
+        {
+
+        key = 5;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+      } //record 5
+  if ((PINB & (1 << PB1)) == 0)//8 is pressed
+      {
+
+        key = 8;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+      } //record 8
+  if ((PINB & (1 << PB2)) == 0) //0 is pressed
+      {
+
+        key = 0;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+
+      } //record 0
+
+
+  PORTD |= 1 << PD7;
+  PORTD &= ~(1 << PD6);
+  PORTB |= 1 << PB0;
+  if ((PIND & (1 << PB5)) == 0) //3 is pressed
+      {
+
+        key = 3;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+      } //record 3
+  if ((PINB & (1 << PB7)) == 0) //6 is pressed
+        {
+
+        key = 6;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+
+
+      } //record 6
+  if ((PINB & (1 << PB1)) == 0) //9 is pressed
+      {
+        key = 9;
+        PORTC |= 1 << PC0;
+        _delay_ms(200);
+        PORTC &= 0 << PC0;
+
+
+      } //record 9
+			if ((PINB & (1 << PB2)) == 0) //# is pressed
+					{
+					key = 12;
+					PORTC |= 1 << PC0;
+					_delay_ms(200);
+					PORTC &= 0 << PC0;
+
+
+					} //record 9
+
+      PORTD &= ~(1 << PD7);
+      PORTD &= ~(1 << PD6);
+      PORTB &= ~(1 << PB0);
+
+
+			if(sendMoneyFlag==1)
+			{
+				if(key == 11)
+				{
+					serial_out(0x2E);
+					key = 10;
+				}
+				if(key==12)
+				{
+					//do nothing
+				}
+				else
+				{
+				 serial_out(key+48);
+				}
+
+
+				//change MUX
+				//send char to PI
+				//Change MUX back
+
+
+			}
+
+}
